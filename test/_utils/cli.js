a91d0b51759525a1892ae8f5fa9fdba972f5686c
LasaleFamine <info@godev.space>
2018-02-14 13:48:32 +0100

    strying npm install for windows tests

--- test/_utils/cli.js
+++ test/_utils/cli.js
@@ -5,6 +5,7 @@ const execa = require('execa');
 const CLI = require.resolve('./../../dist/cli');
 
 const tmpDir = () => resolve(__dirname, '../output', uuid());
+const isWin = process.platform === 'win32';
 
 const run = (command, args, cwd) => {
   return execa(command, args.filter(Boolean), {cwd: cwd || resolve(__dirname)})
@@ -18,7 +19,7 @@ const create = (template, name, install) => {
   const dest = tmpDir();
   const args = [CLI, 'create', template, dest, '--name']
     .concat(name || `test-${template}`)
-    .concat(install ? ['--install', '--yarn'] : '--no-install');
+    .concat(install ? ['--install', isWin ? '--npm' : '--yarn'] : '--no-install');
 
   console.log(
     'Creating skeleton project for testing...',
