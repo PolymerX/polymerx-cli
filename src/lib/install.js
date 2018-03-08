12ec4ad67bbfb8760f785c9575181f0fe5d5cd1a
LasaleFamine <info@godev.space>
2018-03-08 23:24:16 +0100

    trying another way to fix

--- src/lib/install.js
+++ src/lib/install.js
@@ -2,6 +2,5 @@ import execa from 'execa';
 
 export default (cwd, isYarn) => {
   const cmd = isYarn ? 'yarn' : 'npm';
-  const endCmd = process.platform === 'win32' ? `${cmd}.cmd` : cmd;
-  return execa(endCmd, ['install'], {cwd, stdio: 'ignore'});
+  return execa(cmd, ['install'], {cwd, stdio: 'inherit'});
 };
