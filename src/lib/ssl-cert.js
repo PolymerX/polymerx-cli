f943c186ee31f8073bca1fca62e4949dcdf401f3
LasaleFamine <info@godev.space>
2018-09-28 21:08:56 +0200

    Merge branch 'master' into greenkeeper/xo-0.23.0

--- src/lib/ssl-cert.js
+++ src/lib/ssl-cert.js
@@ -5,7 +5,7 @@ import chalk from 'chalk';
 export default () => {
   console.log(chalk.blue('Setting up SSL certificate (may require sudo)...'));
 
-  return getDevelopmentCertificate('preact-cli', {
+  return getDevelopmentCertificate('polymerx-cli', {
     installCertutil: true
   }).catch(error => {
     console.log(chalk.red(`Failed to generate dev SSL certificate: ${error}`));
